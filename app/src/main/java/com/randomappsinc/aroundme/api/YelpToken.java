d291d78ec8fb816d99ccdba82bc7d7bfb2e709ca
Gear61 <chioualexander@yahoo.com>
2021-04-19 17:31:09 -0700

    Just add Yelp token to repo

---
+++ app/src/main/java/com/randomappsinc/aroundme/api/YelpToken.java
@@ -0,0 +1,7 @@
+package com.randomappsinc.aroundme.api;
+
+public class YelpToken {
+
+    public static final String API_KEY = "fGnNv2nmH3cGRbadjIJPKVxHeNHgmM7R6xg33nH8Lq-G7vs3xKSFcY" +
+            "-azb0I20o5_gBLydt5gubpPlP8bK7F1j5bXK-4oJ-AM3KrJPk7id1ix5JXuJKla2hnXg9-YHYx";
+}
